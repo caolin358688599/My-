ee9bc9d57fc5786447d6024ab2e05ebed8ea924a
caolin358688599 <358688599@qq.com>
2018-07-14 21:31:07 +0800

    feat: 完成推荐数据，并且父类

---
+++ DouYu/DouYu/Classes/Main/View/CollectionBaseCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionBaseCell.swift
+//  DouYu
+//
+//  Created by 曹林 on 2018/7/14.
+//  Copyright © 2018年 曹林. All rights reserved.
+//
+
+import UIKit
+
+class CollectionBaseCell: UICollectionViewCell {
+    
+}
