3133f636423d3712f077ceef4e198f00fe94b288
caolin358688599 <358688599@qq.com>
2018-07-15 16:14:03 +0800

    feat: 完成轮播图数据

---
+++ DouYu/DouYu/Classes/Home/View/CollectionCycleCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionCycleCell.swift
+//  DouYu
+//
+//  Created by 曹林 on 2018/7/14.
+//  Copyright © 2018年 曹林. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCycleCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ DouYu/DouYu/Classes/Home/View/RecommendCycleView.swift
@@ -0,0 +1,21 @@
+//
+//  RecommendCycleView.swift
+//  DouYu
+//
+//  Created by 曹林 on 2018/7/14.
+//  Copyright © 2018年 曹林. All rights reserved.
+//
+
+import UIKit
+
+class RecommendCycleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
