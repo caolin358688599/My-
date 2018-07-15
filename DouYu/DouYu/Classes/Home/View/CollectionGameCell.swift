c73b79c7b98edf1157f86261b28499bd753e4697
caolin358688599 <358688599@qq.com>
2018-07-15 17:49:40 +0800

    feat: 完成游戏View

---
+++ DouYu/DouYu/Classes/Home/View/CollectionGameCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionGameCell.swift
+//  DouYu
+//
+//  Created by 曹林 on 2018/7/15.
+//  Copyright © 2018年 曹林. All rights reserved.
+//
+
+import UIKit
+
+class CollectionGameCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ DouYu/DouYu/Classes/Home/View/RecommendGameView.swift
@@ -0,0 +1,21 @@
+//
+//  RecommendGameView.swift
+//  DouYu
+//
+//  Created by 曹林 on 2018/7/15.
+//  Copyright © 2018年 曹林. All rights reserved.
+//
+
+import UIKit
+
+class RecommendGameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
